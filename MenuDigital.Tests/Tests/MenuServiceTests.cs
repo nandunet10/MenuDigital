caf7c5453cf0eace543a22155bcaf2b57eaf5019
susepbr\ba-fernandoos <fernando.santos@susep.gov.br>
2025-02-25 01:06:50 -0300

    (feat) - Criação do projeto de Tests e a implementação dos testes iniciais. #4

---
+++ MenuDigital.Tests/Tests/MenuServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Menu.Models;
+using Menu.Repositories.Interfaces;
+using Menu.Services;
+using Moq;
+
+namespace MenuDigital.Tests.Tests
+{
+    public class MenuServiceTests
+    {
+        private readonly MenuService _menuService;
+        private readonly Mock<IMenuRepository> _menuRepositoryMock;
+
+        public MenuServiceTests()
+        {
+            _menuRepositoryMock = new Mock<IMenuRepository>();
+            _menuService = new MenuService(_menuRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetMenuItems_ShouldReturnMenuItems()
+        {
+            // Arrange
+            var menuItems = new List<MenuItem>
+            {
+                new() { Id = 1, Nome = "Pizza", Preco = 20.00m },
+                new() { Id = 2, Nome = "Hamburguer", Preco = 15.00m }
+            };
+            _menuRepositoryMock.Setup(repo => repo.GetAllMenuItems()).Returns(menuItems);
+
+            // Act
+            var result = _menuService.GetMenuItems();
+
+            // Assert
+            Assert.Equal(2, result.Count());
+        }
+    }
+
+}
---
+++ MenuDigital.Tests/Tests/PedidoServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Menu.Models;
+using Menu.Repositories.Interfaces;
+using Menu.Services;
+using Moq;
+
+namespace MenuDigital.Tests.Tests
+{
+    public class PedidoServiceTests
+    {
+        private readonly PedidoService _pedidoService;
+        private readonly Mock<IPedidoRepository> _pedidoRepositoryMock;
+
+        public PedidoServiceTests()
+        {
+            _pedidoRepositoryMock = new Mock<IPedidoRepository>();
+            _pedidoService = new PedidoService(_pedidoRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void CriarPedido_ShouldReturnPedidoComItens()
+        {
+            // Arrange
+            var menuItems = new List<MenuItem>
+            {
+                new() { Id = 1, Nome = "Pizza", Preco = 20.00m },
+                new() { Id = 2, Nome = "Hamburguer", Preco = 15.00m }
+            };
+
+            var pedidoNovo = new Pedido { Itens = menuItems, Total = menuItems.Sum(item => item.Preco) };
+            _pedidoRepositoryMock.Setup(repo => repo.AdicionarPedido(It.IsAny<Pedido>())).Returns(pedidoNovo);
+
+
+            // Act
+            var pedido = _pedidoService.CriarPedido(menuItems);
+
+            // Assert
+            Assert.NotNull(pedido);
+            Assert.Equal(2, pedido.Itens.Count);
+            Assert.Equal(35.00m, pedido.Total);
+
+            // Verifica se o método AdicionarPedido foi chamado corretamente
+            _pedidoRepositoryMock.Verify(repo => repo.AdicionarPedido(It.IsAny<Pedido>()), Times.Once);
+        }
+    }
+}
