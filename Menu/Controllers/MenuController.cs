cd31281d101ebdb5f45bc791eec4ebba511028b7
susepbr\ba-fernandoos <fernando.santos@susep.gov.br>
2025-02-25 00:31:58 -0300

    (feat) - Adicionando controladores. #3

---
+++ Menu/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using Menu.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Menu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController(MenuService menuService) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMenuItems()
+        {
+            var menu = menuService.GetMenuItems();
+            return Ok(menu);
+        }
+    }
+
+}
---
+++ Menu/Controllers/PedidoController.cs
@@ -0,0 +1,40 @@
+﻿using Menu.Models;
+using Menu.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Menu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController(PedidoService pedidoService) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult BuscarPedido(int id)
+        {
+            var pedido = pedidoService.BuscarPedido(id);
+            return Ok(pedido);
+        }
+
+        [HttpPost]
+        public IActionResult CriarPedido([FromBody] List<MenuItem> itens)
+        {
+            var pedido = pedidoService.CriarPedido(itens);
+            return Ok(pedido);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult CancelarPedido(int id)
+        {
+            pedidoService.CancelarPedido(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditarPedido(int id, [FromBody] List<MenuItem> itens)
+        {
+            pedidoService.EditarPedido(id, itens);
+            return NoContent();
+        }
+    }
+
+}
